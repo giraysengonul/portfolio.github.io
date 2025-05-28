820a199a2568f0478a9f8d036ef79e5469b36eb1
giraysengonul <giraysengonul@gmail.com>
2025-05-29 01:07:44 +0300

    certificates added.

--- script.js
+++ script.js
@@ -7,3 +7,21 @@ function toggleMenu() {
 document.querySelectorAll('a[href^="http"]').forEach(link => {
   link.setAttribute('target', '_blank');
 });
+ function openPopup(imageSrc) {
+    document.getElementById("popupImage").src = imageSrc;
+    document.getElementById("certificatePopup").style.display = "flex";
+  }
+
+  function closePopup() {
+    document.getElementById("certificatePopup").style.display = "none";
+  }
+  function openPopup(imageUrl) {
+  const popup = document.getElementById('certificatePopup');
+  const popupImage = document.getElementById('popupImage');
+  popupImage.src = imageUrl;
+  popup.style.display = 'flex';
+}
+
+function closePopup() {
+  document.getElementById('certificatePopup').style.display = 'none';
+}
